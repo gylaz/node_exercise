7654e09b2a916555c3c575151173a9b6c753d834
Greg Lazarev <greg.lazarev@gmail.com>
2022-09-01 14:12:51 -0700

    Update default route response text
    For clarity that it's not a generic message

--- router.js
+++ router.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('API IS UP!'));
+router.get('/', (req, res) => res.send('ADVICE API IS ONLINE!'));
 
 export default router;
